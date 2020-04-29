d6ddd0ea51e2c20c67ad4bcc2fa123802356e017
hila5 <hila5@post.bgu.ac.il>
2020-04-30 01:30:44 +0300

    fix registering form

--- js/usersContainer.js
+++ js/usersContainer.js
@@ -2,14 +2,17 @@ var userP = {Username:'p', FirstName:'p', LastName:"p", Email:"p@gmail.com" ,Pas
 var usersArray=[];
 usersArray.push(userP);
 var userFullName;
+var alphanumericReg=/^[a-zA-Z0-9]+$/;
+var alphaReg=/^[a-zA-Z]+$/;
+var emailReg=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 $(document).ready(function () {
     jQuery.validator.addMethod("alphanumeric", function(value, element) {
-        return this.optional(element) ||/^[a-zA-Z0-9]+$/.test(value);
+        return this.optional(element) ||alphanumericReg.test(value);
     }, "Please try to input only letters and numbers");
 
     jQuery.validator.addMethod("alpha", function(value, element) {
-        return this.optional(element) || /^[a-zA-Z]+$/.test(value);
+        return this.optional(element) || alphaReg.test(value);
     }, "Please try to input only letters");
 
     $("#RegisterForm").validate({
@@ -39,19 +42,53 @@ $(document).ready(function () {
     });
   });
 
+
+function alphaTest(input) {
+    if (alphaReg.test(input)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+ function emailTest(input) {
+    if (emailReg.test(input)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+ function alphanumericTest(input) {
+    if (input.length >= 6 && alphanumericReg.test(input)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function registerIn(){
     var Username = document.getElementById("Username_rgs").value;
-        var Password = document.getElementById("Password_").value;
-        var FirstName = document.getElementById("FirstName_").value;
-        var LastName = document.getElementById("LastName_").value;
-        var Email = document.getElementById("Email_").value;
-        var days = document.getElementById("day").value;
-        var months = document.getElementById("month").value;
-        var years = document.getElementById("year").value;
+    var Password = document.getElementById("Password_").value;
+    var FirstName = document.getElementById("FirstName_").value;
+    var LastName = document.getElementById("LastName_").value;
+    var Email = document.getElementById("Email_").value;
+    var days = document.getElementById("day").value;
+    var months = document.getElementById("month").value;
+    var years = document.getElementById("year").value;
+
+    if(alphaTest(FirstName)&&alphaTest(LastName)&&emailTest(Email)&&alphanumericTest(Password)){
         usersArray.push({Username,FirstName,LastName,Email,Password,days,months, years});
         welcome();
-        var form = document.getElementById("RegisterForm")
-        form.reset();
+    }
+    else{
+        alert("try to enter details again!");
+    }
+     var form = document.getElementById("RegisterForm");
+     form.reset();
 }
 
 function loginSettings(){
@@ -62,9 +99,11 @@ function loginSettings(){
             settings_nav();
         }
         else{
-            alert("wrong input!")
+            alert("wrong input!");
             login();
         }
+        var form = document.getElementById("LoginForm");
+        form.reset();
 }
 
 function getFullName(username) {
